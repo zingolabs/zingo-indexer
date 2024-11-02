5f112f93ced4e72dd138ced5f9a2953f425dd038
Oscar Pepper <oscarpepper@zingolabs.org>
2024-11-02 07:30:42 +0000

    integrated zcash local net test fixtures into zaino tests

---
+++ integration-tests/tests/client_rpcs.rs
@@ -0,0 +1,478 @@
+//! Tests Zainod release binary against the `zcash_local_net` client RPC test fixtures.
+//!
+//! Pre-requisites:
+//! - Run `cargo build --release` to update to latest release binary
+//! - Run `generate_zcashd_chain_cache` to generate the chain cache for testing.
+
+use std::path::PathBuf;
+
+use once_cell::sync::Lazy;
+
+const ZCASHD_BIN: Option<PathBuf> = None;
+const ZCASH_CLI_BIN: Option<PathBuf> = None;
+const LIGHTWALLETD_BIN: Option<PathBuf> = None;
+static ZAINOD_BIN: Lazy<Option<PathBuf>> = Lazy::new(|| {
+    let mut workspace_root_path = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    workspace_root_path.pop();
+    Some(workspace_root_path.join("target/release/zainod"))
+});
+
+#[ignore = "not a test. generates chain cache for client_rpc tests."]
+#[tokio::test]
+async fn generate_zcashd_chain_cache() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::generate_zcashd_chain_cache(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_lightd_info() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_lightd_info(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_latest_block() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_latest_block(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_block() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_block(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_block_out_of_bounds() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_block_out_of_bounds(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_block_nullifiers() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_block_nullifiers(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_block_range_nullifiers() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_block_range_nullifiers(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_block_range_nullifiers_reverse() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_block_range_nullifiers_reverse(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_block_range_lower() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_block_range_lower(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_block_range_upper() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_block_range_upper(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_block_range_reverse() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_block_range_reverse(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_block_range_out_of_bounds() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_block_range_out_of_bounds(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_transaction() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_transaction(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[ignore = "incomplete"]
+#[tokio::test]
+async fn send_transaction() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::send_transaction(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_taddress_txids_all() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_taddress_txids_all(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_taddress_txids_lower() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_taddress_txids_lower(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_taddress_txids_upper() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_taddress_txids_upper(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_taddress_balance() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_taddress_balance(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_taddress_balance_stream() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_taddress_balance_stream(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_mempool_tx() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_mempool_tx(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_mempool_stream() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_mempool_stream(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_tree_state_by_height() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_tree_state_by_height(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_tree_state_by_hash() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_tree_state_by_hash(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_tree_state_out_of_bounds() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_tree_state_out_of_bounds(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_latest_tree_state() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_latest_tree_state(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+// this is not a satisfactory test for this rpc and will return empty vecs.
+// this rpc should also be tested in testnet/mainnet or a local chain with at least 2 shards should be cached.
+#[tokio::test]
+async fn get_subtree_roots_sapling() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_subtree_roots_sapling(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+// this is not a satisfactory test for this rpc and will return empty vecs.
+// this rpc should also be tested in testnet/mainnet or a local chain with at least 2 shards should be cached.
+#[tokio::test]
+async fn get_subtree_roots_orchard() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_subtree_roots_orchard(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_address_utxos_all() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_address_utxos_all(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_address_utxos_lower() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_address_utxos_lower(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_address_utxos_upper() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_address_utxos_upper(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_address_utxos_out_of_bounds() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_address_utxos_out_of_bounds(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_address_utxos_stream_all() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_address_utxos_stream_all(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_address_utxos_stream_lower() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_address_utxos_stream_lower(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_address_utxos_stream_upper() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_address_utxos_stream_upper(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn get_address_utxos_stream_out_of_bounds() {
+    tracing_subscriber::fmt().init();
+
+    zcash_local_net::test_fixtures::get_address_utxos_stream_out_of_bounds(
+        ZCASHD_BIN,
+        ZCASH_CLI_BIN,
+        ZAINOD_BIN.clone(),
+        LIGHTWALLETD_BIN,
+    )
+    .await;
+}
